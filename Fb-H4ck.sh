8548d93b2de149cb785dd2235df01ecb911d8642
tukangpulung199 <52864713+tukangpulug199@users.noreply.github.com>
2019-07-15 19:51:59 +0700

    Add files via upload

---
+++ Fb-H4ck.sh
@@ -0,0 +1,212 @@
+#!/usr/bin/bash
+clear
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# AC1LDAY127
+echo
+echo
+#ini paket untuk menjalankan tools
+t.sh
+echo
+clear
+echo
+echo $me"       ,;~'             '~;,"
+echo $me"      ,;                     ;,"
+echo $me"    ;                         ;"
+echo $me"   ,'                         ',"
+echo $me"  ,;                           ;,"
+echo $me"  ; ;      .           .      ; ;"
+echo $me"  | ;   ______       ______   ; |"
+echo $me"  |  '/~'     ~' . '~     '~\'  |"
+echo $me"  |  ~  ,-~~~^~, | ,~^~~~-,  ~  |"
+echo $me"   |"$me"   |        }:{        |   |"
+echo $me"   |"$me"   l       / | \       !   |"
+echo $me"   .~"$me"  (__,.--' .^. '--.,__)  ~."
+echo $cy"   |     ---;' / | \ ';---     |"
+echo $cy"     \__.       \/^\/       .__/"
+echo $cy"    V| \                 / |V"
+echo $ku"      | |T~\___!___!___/~T| |"
+echo $ku"      | |'IIII_I_I_I_IIII'| |"
+echo $ku"      |  \,III I I I III,/  |"
+echo $cy"        \   '~~~~~~~~~~'    /"
+echo $cy"         \   .       .   /"
+echo $cy"          \.    ^    ./"
+echo $cy"            ^~~~^~~~^"
+sleep 6
+echo
+echo $cy" [✓]MERAHPUTIH"
+echo $me"■█■█■█■█■█■█■█■■█■█■"$pu"█■█■█■█■█■■█■█■█■█■█■█"
+echo $me "AUTHOR   : AC1L D4Y"
+echo $i  "WA       : 085280645068"
+echo $i  "YOUTUBE  : MR 4444"
+echo $i  "Version  : Tools ini Versi 1.2"
+echo $pu "NOTE : JIKA ADA MASALAH HUBUNGI PEMBUAT SCRIP"
+echo $pu"■█■█■█■█■█■█■█■■█■█■"$me"█■█■█■█■█■■█■█■█■█■█■█"
+sleep 4
+echo
+echo $pu"               ╭∩╮（￣▽￣）╭∩╮"
+echo $me"║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌"
+echo $pu"     "$me"   [•]     [1] HACK FB    "$me"  [•]      "
+echo $pu"     "$ku"   [•]     [2]HACK GMAIL  "$ku"  [•]    "
+echo $pu"     "$i"  [•]      [3]HACK IG     "$i"  [•]    "
+echo $pu"     "$bi"   [•]     [4]DEFACEEE    "$bi"  [•]    "
+echo $pu"║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌│█│║▌║║▌│█║▌│ █║▌"
+echo $cy"┌==="$bi"[ ACIL D4YY ]"
+echo $cy"¦"
+read -p">>>" pil;
+if [ $pil = 1 ]
+then
+clear
+echo $me"╔╗─╔╗╔═══╗╔═══╗╔╗╔═╗╔═══╗╔═══╗╔═══╗╔═══╗╔══╗─╔═══╗╔═══╗╔╗╔═╗"
+echo $me"║║─║║║╔═╗║║╔═╗║║║║╔╝║╔══╝║╔═╗║║╔═╗║║╔══╝║╔╗║─║╔═╗║║╔═╗║║║║╔╝"
+echo $me"║╚═╝║║║─║║║║─╚╝║╚╝╝─║╚══╗║║─║║║║─╚╝║╚══╗║╚╝╚╗║║─║║║║─║║║╚╝╝─"
+echo $pu"║╔═╗║║╚═╝║║║─╔╗║╔╗║─║╔══╝║╚═╝║║║─╔╗║╔══╝║╔═╗║║║─║║║║─║║║╔╗║─"
+echo $pu"║║─║║║╔═╗║║╚═╝║║║║╚╗║║───║╔═╗║║╚═╝║║╚══╗║╚═╝║║╚═╝║║╚═╝║║║║╚╗"
+echo $pu"╚╝─╚╝╚╝─╚╝╚═══╝╚╝╚═╝╚╝───╚╝─╚╝╚═══╝╚═══╝╚═══╝╚═══╝╚═══╝╚╝╚═╝-"
+sleep 4
+echo $me"■█■█■█■█■█■█■█■■█■█■█■█■█■█■█■■█■█■█■█■█■█"
+echo $i     "Author : ACIL D4Y 127"
+echo $i     "WA : 085280645068"
+echo $i     "Tools : Untuk Hack Facebook"
+echo $pu"■█■█■█■█■█■█■█■■█■█■█■█■█■█■█■■█■█■█■█■█■█"
+sleep 4
+echo
+read -p "Masukan nama Facebook Korban: " script;
+echo "Loading....." | lolcat
+sleep 4
+echo "Membuka Pengaturan facebook" | lolcat
+echo "Loading....."
+sleep 4
+echo "Berhasil mencury Data korban" | lolcat
+echo "=============================="
+echo $me"Nama Facebook : Farhan"
+echo $cy"Password : farhanmerdeka"
+echo "=============================="
+echo $me"NOTE : JIKA ADA MASALAH HUBUNGI AUTHOR"
+echo $i"Terimakasih telah menggunakan tools ini"
+fi
+
+if [ $pil = 2 ]
+then
+clear
+clear#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# AC1LDAY127
+echo
+echo
+#ini paket untuk menjalankan tools
+t.sh
+echo
+clear
+echo
+echo
+echo $me" █░░ ▄▀▄ ▄▀ █░▄▀ ▄▀▀░ █▄░▄█ ▄▀▄ ▀ █░░"
+echo $i" █▀▄ █▀█ █░ █▀▄░ █░▀▌ █░█░█ █▀█ █ █░▄"
+echo $cy" ▀░▀ ▀░▀ ░▀ ▀░▀▀ ▀▀▀░ ▀░░░▀ ▀░▀ ▀ ▀▀▀"
+echo $me "========================="
+echo $i  "AUTHOR : ACIL D4Y 127"
+echo $cy "WA : +62852-8064-5068"
+echo $i  "Youtube : MR 4444"
+echo $me "========================="
+echo "Selamat Datang Di Tools Hack Gmail"
+echo "Loading....." | lolcat
+read -p "Masukan username GMAIL korban : " script;
+sleep 4
+echo "Akun gmail Sedang di retas Harap TUNGGU!" | lolcat
+sleep 4
+sleep 2
+echo "Akun berhasil di retas"
+echo "========================"
+echo $i "Password : j1j88b4k0"
+echo "========================"
+echo $me "NOTE JIKA ADA MASALAH HUBUNGI AUTHOR"
+echo "Terimakasih telah menggunakan tools kami:))" | lolcat
+fi
+
+if [ $pil = 3 ]
+then
+clear
+clear#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# AC1LDAY127
+echo
+echo
+#ini paket untuk menjalankan tools
+t.sh
+echo
+clear
+echo
+echo
+echo $me" █── ▄▀▄ ▄▀ █─▄▀ ▀ █▄─█ ▄▀▀ ▀█▀ ▄▀▄ ▄▀▀─ █▀▀▄ ▄▀▄ █▄─▄█"
+echo $i "█▀▄ █▀█ █─ █▀▄─ █ █─▀█ ─▀▄ ─█─ █▀█ █─▀▌ █▐█▀ █▀█ █─█─█"
+echo $me" ▀─▀ ▀─▀ ─▀ ▀─▀▀ ▀ ▀──▀ ▀▀─ ─▀─ ▀─▀ ▀▀▀─ ▀─▀▀ ▀─▀ ▀───▀"
+echo $i "======================"
+echo $cy"Author : ACIL D4Y 127"
+echo $cy"Youtube : MR 4444"
+echo $i "======================"
+echo "Selamat Datang Di Tools Hack instagram"
+echo "Loading...."
+sleep 4
+read -p "Masukan username/nama instagram : " script;
+echo "Loading......"
+sleep 4
+echo "Akun instagram Sedang di retas Harap TUNGGU!" | lolcat
+sleep 4
+sleep 2
+echo "Akun berhasil di retas"
+sleep 4
+echo "========================"
+echo $me "Password : supriyatna02827"
+echo "========================"
+echo $me "NOTE JIKA ADA MASALAH HUBUNGI AUTHOR"
+echo "Terimakasih gan RESIKO DI TANGGUNG SENDIRI" | lolcat
+fi
+
+if [ $pil = 4 ]
+then
+clear
+clear#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# AC1LDAY127
+echo
+echo
+#ini paket untuk menjalankan tools
+t.sh
+echo
+clear
+echo
+echo
+echo $pur"──╔╗─────╔═╗──────────────────────╔╗──"
+echo $pur"──║║─────║╔╝──────────────────────║║──"
+echo $me "╔═╝║╔══╗╔╝╚╗╔══╗╔══╗╔══╗╔╗╔╗╔╗╔══╗║╚═╗"
+echo $me "║╔╗║║║═╣╚╗╔╝║╔╗║║╔═╝║║═╣║╚╝╚╝║║║═╣║╔╗║"
+echo $cy "║╚╝║║║═╣─║║─║╔╗║║╚═╗║║═╣╚╗╔╗╔╝║║═╣║╚╝║"
+echo $cy "╚══╝╚══╝─╚╝─╚╝╚╝╚══╝╚══╝─╚╝╚╝─╚══╝╚══╝"
+echo $pur"______________________________________"
+echo ""
+echo $pu "MAAF TOOLS DEFACEWEB SEDANG UPDATE:)"
+fi
